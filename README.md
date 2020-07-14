fa2dd7a64c22ea120ef8fbe6f6e3744a8d25ce7f
Maxwel <maxpdr96@gmail.com>
2020-07-14 20:52:36 -0300

    Creating scope of the To-do project

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ReduxTodoapp
 
+To-do List using Angular and NgRx.
+
+## Angular 
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.2.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { FooterComponent } from './footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { TodoModule } from './todos/todo.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
